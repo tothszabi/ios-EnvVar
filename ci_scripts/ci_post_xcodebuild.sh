ce109dcfc6012b220f1ffd146e80ebdf21d6b7fb
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2024-06-26 13:19:37 +0200

    Update CI scripts

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -2,11 +2,19 @@
 
 set -ex
 
-echo ""
-cat "/Volumes/workspace/ci/development-exportoptions.plist"
-echo ""
-cat "/Volumes/workspace/ci/ad-hoc-exportoptions.plist"
-echo ""
-cat "/Volumes/workspace/ci/app-store-exportoptions.plist"
-echo ""
+if [[ -f "/Volumes/workspace/ci/development-exportoptions.plist" ]]; then
+    echo ""
+    cat "/Volumes/workspace/ci/development-exportoptions.plist"
+fi
+
+if [[ -f "/Volumes/workspace/ci/ad-hoc-exportoptions.plist" ]]; then
+    echo ""
+    cat "/Volumes/workspace/ci/ad-hoc-exportoptions.plist"
+fi
+
+if [[ -f "/Volumes/workspace/ci/app-store-exportoptions.plist" ]]; then
+    echo ""
+    cat "/Volumes/workspace/ci/app-store-exportoptions.plist"
+fi
+
 exit 0
